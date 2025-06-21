8cc66f79e06e80dede43789e9e78316c75951492
moisesliotawork <moises.liotawork@gmail.com>
2025-06-21 11:43:36 -0400

    Creacion del modelo para la sesiones de trabajo :sparkles:

---
+++ app/Models/WorkSession.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WorkSession extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'start_time',
+        'end_time',
+        'latitude',
+        'longitude',
+        'ip_address',
+        'device_info'
+    ];
+
+    protected $casts = [
+        'start_time' => 'datetime',
+        'end_time' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->whereNull('end_time');
+    }
+
+    public function isActive()
+    {
+        return is_null($this->end_time);
+    }
+}
