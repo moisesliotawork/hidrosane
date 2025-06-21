55f91cf314c3ad565e97822ebe3a97c33cd059c2
moisesliotawork <moises.liotawork@gmail.com>
2025-06-21 13:46:18 -0400

    configuracion inicial de panel de teleoperador :sparkles:

---
+++ app/Providers/Filament/TeleoperatorPanelProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\AuthenticateSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use App\Http\Middleware\StartWorkSession;
+use App\Filament\Widgets\ActiveWorkSessionWidget;
+
+class TeleoperatorPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->id('teleoperador')
+            ->path('teleoperador')
+            ->login()
+            ->brandName("Ohana")
+            ->colors([
+                'primary' => Color::Lime,
+            ])
+            ->discoverResources(in: app_path('Filament/Teleoperator/Resources'), for: 'App\\Filament\\Teleoperator\\Resources')
+            ->discoverPages(in: app_path('Filament/Teleoperator/Pages'), for: 'App\\Filament\\Teleoperator\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Teleoperator/Widgets'), for: 'App\\Filament\\Teleoperator\\Widgets')
+            ->widgets([
+                ActiveWorkSessionWidget::class,
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                StartWorkSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ]);
+    }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\CommercialPanelProvider::class,
     App\Providers\Filament\HeadOfRoomPanelProvider::class,
+    App\Providers\Filament\TeleoperatorPanelProvider::class,
 ];
