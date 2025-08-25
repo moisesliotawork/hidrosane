1910bc7d9f1dc1a71ce76f1e8fb9b335fa5d2791
moisesliotawork <moises.liotawork@gmail.com>
2025-08-25 05:59:23 -0400

    update horarios:

--- app/Enums/HorarioNotas.php
+++ app/Enums/HorarioNotas.php
@@ -8,6 +8,7 @@ use Filament\Support\Contracts\HasColor;
 enum HorarioNotas: string implements HasLabel
 {
     case _11_13 = '11-13';
+    case _10_12 = '10-12';
     case _12_14 = '12-14';
     case _13_1430 = '13-14:30';
     case TM = 'TM';
@@ -22,6 +23,7 @@ enum HorarioNotas: string implements HasLabel
     {
         return match ($this) {
             self::_11_13 => '11-13',
+            self::_10_12 => '10-12',
             self::_12_14 => '12-14',
             self::_13_1430 => '13-14:30',
             self::TM => 'TM',
